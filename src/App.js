ec70aa35b3b893e3b36a354c8a0d321224993d8e
jtwray <tuckerwray84@gmail.com>
2019-11-14 00:01:50 -0500

    starts on a removeitem button

--- src/App.js
+++ src/App.js
@@ -14,34 +14,45 @@ import {CartContext} from './contexts/CartContext';
 
 function App() {
 	const [products]=useState( data );
-	const [cart, setCart]=useState( [] );
+	const [cart, setCart]=useState( [] ); 
 
 	const addItem=item => {
 		setCart( [...cart, item] )
 	};
+	const removeItem=item => {
+	let filteredItems=cart.filter((cartMovie)=>{return (cartMovie.id!==item.id)})	
+	
+		setCart(filteredItems)
+		console.log("cart:",cart)
+
+	}
+	// const removeFromSavedList=movie => {
+	// 	const filteredList=savedList.filter( savedListMovie => savedListMovie.id!==movie.id )
+	
+	// 	setSavedList( filteredList )
 
 	return (
 		<ProductContext.Provider value={{products, addItem}}>
-		<CartContext.Provider value={{cart}}>
+			<CartContext.Provider value={{cart, removeItem}}>
 
 
-			<div className="App">
-				<Navigation cart={cart} />
+				<div className="App">
+					<Navigation />
 
-				{/* Routes */}
-				<Route
-					exact
-					path="/"
-					component={Products}
-				/>
+					{/* Routes */}
+					<Route
+						exact
+						path="/"
+						component={Products}
+					/>
 
-				<Route
-					path="/cart"
-					component={ShoppingCart}
-				/>
+					<Route
+						path="/cart"
+						component={ShoppingCart}
+					/>
 
-			</div>
-		</CartContext.Provider>
+				</div>
+			</CartContext.Provider>
 		</ProductContext.Provider>
 	);
 }
--- src/components/ShoppingCartItem.js
+++ src/components/ShoppingCartItem.js
@@ -1,6 +1,13 @@
-import React from 'react';
+import React,{useContext} from 'react';
+
+//contexts
+import{CartContext} from './../contexts/CartContext'
 
 const Item = props => {
+	const {removeItem,cart}=useContext(CartContext)
+	const{...item}=props
+
+
 	return (
 		<div className="shopping-cart_item">
 			<img src={props.image} alt={`${props.title} book`} />
@@ -9,7 +16,7 @@ const Item = props => {
 			<div>
 				<h1>{props.title}</h1>
 				<p>$ {props.price}</p>
-				<button>Remove from cart</button>
+				<button onClick={removeItem}>Remove from cart</button>
 			</div>
 		</div>
 	);
